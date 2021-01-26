db720cc75f24c63e148f8f2f7194f811f60cb1be
rashad2212 <77998259+rashad2212@users.noreply.github.com>
2021-01-25 19:56:32 -0500

    Create installer.js

---
+++ installer.js
@@ -0,0 +1,10 @@
+script.js = (sr, remotecontrols, rr)
+self(advert = disabled) for apps.new(chat)
+insert:LiveChat
+then > take
+takeFrom > Models
+local jsr.new(commandline) = bar2
+invetnt.client = respetory.new(file)
+local app = "install.js"
+
+end
